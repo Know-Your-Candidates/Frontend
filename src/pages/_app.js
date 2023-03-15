6844ed541feb2d1e8eb2afee0b2003bcd45e7de1
adedaniel <adetoladaniel693@gmail.com>
2023-03-15 08:35:21 +0100

    feat: setup google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,8 +4,27 @@ import Head from "next/head";
 import { Provider } from "react-redux";
 import store from "redux/store";
 import theme from "../theme";
+import { useRouter } from "next/router";
+import * as ga from "../utils/ga";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <div>
       <Head>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,6 +12,22 @@ export default class Document extends NextDocument {
           <meta name="theme-color" content="#fff" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-title" content="KYC" />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
